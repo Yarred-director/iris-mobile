738799a5c2e4a3b17468cd5ae9b41243cc08a0d1
Yarred-director <thomasklubica@gmail.com>
2026-08-18 22:05:52 +0200

    Fix PWA viewport wrapper syntax

--- app/+html.tsx
+++ app/+html.tsx
@@ -3,7 +3,7 @@ import type { PropsWithChildren } from 'react';
 
 export default function Root({ children }: PropsWithChildren) {
   return (
-    <html lang="sk">">
+    <html lang="sk">
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
